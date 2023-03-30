69002f6fe30e8c6ad0964a829cbbc253d8a923aa
Baddyo <qianjunyinggo@gmail.com>
2023-03-31 00:38:18 +0800

    feature: 接入 React  工程

--- src/appProvider.ts
+++ src/appProvider.ts
@@ -1,9 +1,12 @@
 import * as vscode from 'vscode'
+// const fs = require('fs')
+import * as fs from 'fs'
+import * as path from 'path'
 export class AppProvider implements vscode.WebviewViewProvider {
   public static readonly viewType = 'Yalper.app'
   constructor(private readonly _context:vscode.ExtensionContext) {
   }
-  public resolveWebviewView(
+  public async resolveWebviewView(
     webviewView: vscode.WebviewView,
     context: vscode.WebviewViewResolveContext,
     token: vscode.CancellationToken
@@ -15,8 +18,12 @@ export class AppProvider implements vscode.WebviewViewProvider {
         ]
       }
       // app.js 是 React 组件编译后的产物，作为 Webview 中的引入脚本
-      // const appPath = vscode.Uri.joinPath(this._context.extensionUri, 'out', 'app.js')
-      // const app = webviewView.webview.asWebviewUri(appPath)
+      const buildPath = 'src/app/build'
+      const { jsName, cssName } = await getBuiltName(buildPath)
+      const JSPath = vscode.Uri.joinPath(this._context.extensionUri, buildPath, jsName)
+      const appJSPath = webviewView.webview.asWebviewUri(JSPath)
+      const CSSPath = vscode.Uri.joinPath(this._context.extensionUri, buildPath, cssName)
+      const appCSSPath = webviewView.webview.asWebviewUri(CSSPath)
       
       webviewView.webview.html = `
         <!DOCTYPE html>
@@ -25,6 +32,8 @@ export class AppProvider implements vscode.WebviewViewProvider {
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Yalper WebView</title>
+            <script defer="defer" src="${appJSPath}"></script>
+            <link href="${appCSSPath}" rel="stylesheet">
           </head>
           <body>
             <div id="root">XXXXX</div>
@@ -32,6 +41,28 @@ export class AppProvider implements vscode.WebviewViewProvider {
         </html>
       `
 
-      // <script src="${app}"></script>
+      // <link href="${appCSSPath}" rel="stylesheet">
   }
+}
+const getBuiltName = (buildPath:String) => {
+  // const filePath = `/${buildPath}/asset-manifest.json`;
+  const filePath = path.resolve(__dirname, `../${buildPath}/asset-manifest.json`)
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', function (err, data) {
+      // 处理错误
+      if (err) {
+        reject(`Error reading file: ${err}`);
+        return;
+      }
+      
+      // 打印文件内容
+      console.log('File content:');
+      const obj = JSON.parse(data)
+      console.log(obj);
+      resolve({
+        jsName: obj.files['main.js'],
+        cssName: obj.files['main.css']
+      })
+    });
+  });
 }
